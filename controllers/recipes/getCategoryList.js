63983ad0058d08b51669071ddf25730dda2acc74
Oleh Pavlenko <a310thunderbolt@gmail.com>
2023-04-01 22:00:17 +0300

    adds controller getCategoryList

--- controllers/recipes/getCategoryList.js
+++ controllers/recipes/getCategoryList.js
@@ -1,3 +1,21 @@
-const getCategoryList = async (req, res) => {};
+const { Recipe } = require("../../models/recipe");
+
+const getCategoryList = async (req, res) => {
+  const allRecipesCategoryList = [];
+
+  const baseResult = await Recipe.find({}, "category -_id").sort({
+    category: 1,
+  });
+
+  baseResult.map((item) => {
+    allRecipesCategoryList.push(item["category"]);
+  });
+
+  const uniqueCategoryList = allRecipesCategoryList.filter(
+    (category, index, array) => array.indexOf(category) === index
+  );
+
+  return res.status(200).json(uniqueCategoryList);
+};
 
 module.exports = getCategoryList;
