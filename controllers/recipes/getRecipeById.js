2d9a34bbbc1bf304900ac1455922fa9c61f173bc
IrynaKholod <104202392+IrynaKholod@users.noreply.github.com>
2023-04-02 14:24:23 +0300

    add getRecipeById

--- controllers/recipes/getRecipeById.js
+++ controllers/recipes/getRecipeById.js
@@ -1,3 +1,17 @@
-const getRecipeById = async (req, res) => {};
+const { Recipe } = require("../../models/recipe");
+const { HttpError } = require("../../helpers");
+
+const getRecipeById = async (req, res) => {
+  const { id } = req.params;
+  const recepie = await Recipe.findById(id);
+
+  if (!recepie) throw HttpError(404, "Not found");
+
+  res.status(200).json({
+    data: {
+      recepie,
+    },
+  });
+};
 
 module.exports = getRecipeById;
