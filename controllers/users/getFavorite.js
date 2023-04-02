008ab1db2af74f244b4a39a3b40e91e2a260fc75
Betelgeuse22 <Don.vitaki@gmail.com>
2023-04-02 20:06:38 +0200

    getFavorite

--- controllers/users/getFavorite.js
+++ controllers/users/getFavorite.js
@@ -1,4 +1,17 @@
-const getFavorite = async (req, res) => {};
+const { User } = require("../../models/user");
+const { HttpError } = require("../../helpers");
+
+const getFavorite = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { userId } = req.params;
+
+  const result = await User.findOne({ _id: userId, owner });
+
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result.favorites);
+};
 // получить все "избранные" рецепты
 
 module.exports = getFavorite;
