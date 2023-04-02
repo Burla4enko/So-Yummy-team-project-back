76bc688cae58ed5c12b085ada9d6a9081d032ef7
Andrew Burlachenko <ap.burla4enko@gmail.com>
2023-04-02 21:09:13 +0300

    added Joi schema

--- controllers/recipes/getIngredientsList.js
+++ controllers/recipes/getIngredientsList.js
@@ -1,7 +1,7 @@
 const { Ingredient } = require("../../models/ingredient");
 
 const getIngredientsList = async (req, res) => {
-  const { page = 1, limit = 999 } = req.query;
+  const { page = 1, limit = 50 } = req.query;
   const skip = (page - 1) * limit;
   const result = await Ingredient.find({}, "", { skip, limit });
   res.json(result);
