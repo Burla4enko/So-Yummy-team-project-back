f3651bbdf5327027a959b9db159cdfe431359218
Andrew Burlachenko <ap.burla4enko@gmail.com>
2023-04-02 21:18:41 +0300

    Update getRecipeById.js

--- controllers/recipes/getRecipeById.js
+++ controllers/recipes/getRecipeById.js
@@ -3,13 +3,13 @@ const { HttpError } = require("../../helpers");
 
 const getRecipeById = async (req, res) => {
   const { id } = req.params;
-  const recepie = await Recipe.findById(id);
+  const recipe = await Recipe.findById(id);
 
-  if (!recepie) throw HttpError(404, "Not found");
+  if (!recipe) throw HttpError(404, "Not found");
 
   res.status(200).json({
     data: {
-      recepie,
+      recipe,
     },
   });
 };
