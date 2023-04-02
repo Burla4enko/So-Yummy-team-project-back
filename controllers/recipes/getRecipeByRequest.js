591469ec5ff0479190000faead8b0585b34df2ef
Vsevolod1001 <sevant.101@gmail.com>
2023-04-02 14:18:04 +0300

    выполнен поиск только по title в Recipe

--- controllers/recipes/getRecipeByRequest.js
+++ controllers/recipes/getRecipeByRequest.js
@@ -1,3 +1,12 @@
-const getRecipeByRequest = async (req, res) => {};
+const { Recipe } = require("../../models/recipe");
+
+const getRecipeByRequest = async (req, res) => {
+  const { query } = req.query;
+  const searchRecipe = await Recipe.find({ $text: { $search: query } });
+
+  return res.json({
+    searchRecipe,
+  });
+};
 
 module.exports = getRecipeByRequest;
