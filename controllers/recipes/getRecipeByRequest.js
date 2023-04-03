34842c015ece2194fa45609594cf4b4614214eeb
Vsevolod1001 <sevant.101@gmail.com>
2023-04-03 15:51:59 +0300

    добавлена проверка на поиск ингридиентов, и поиск рецептов

--- controllers/recipes/getRecipeByRequest.js
+++ controllers/recipes/getRecipeByRequest.js
@@ -1,9 +1,12 @@
 const { Recipe } = require("../../models/recipe");
+const { HttpError } = require("../../helpers");
 
 const getRecipeByRequest = async (req, res) => {
   const { query } = req.query;
   const searchRecipe = await Recipe.find({ $text: { $search: query } });
-
+  if (searchRecipe.length === 0) {
+    throw HttpError(404, "recipe not found");
+  }
   return res.json(searchRecipe);
 };
 
