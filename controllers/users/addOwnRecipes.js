269128de23ac4e1423ee441c95be4f7722139ca9
Bordiuzhan <bordyujan@gmail.com>
2023-04-03 14:27:00 +0200

    add ingredients to recipe

--- controllers/users/addOwnRecipes.js
+++ controllers/users/addOwnRecipes.js
@@ -1,15 +1,28 @@
 const { Recipe } = require('../../models/recipe');
 const { Ingredient } = require('../../models/ingredient');
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Types;
 
 const addOwnRecipes = async (req, res) => {
   const { _id: owner } = req.user;
   const { path } = req.file;
-  const { ingredients, ...recipe } = req.body;
-  console.log(...ingredients);
-  const ingredientList = await Ingredient.find({ ...ingredients });
-  //   console.log(ingredientList);
-  const result = await Recipe.create({ owner, thumb: path, ...recipe });
+  const { ...recipe } = req.body;
+  const ingredients = [
+    '640c2dd963a319ea671e377f',
+    '640c2dd963a319ea671e36e4',
+    '640c2dd963a319ea671e377e',
+  ]; // потім деструктуризувати з req.body дивлячись які дані будуть приходити//
+  const ingredientsList = await Ingredient.find({
+    _id: { $in: ingredients.map((id) => new ObjectId(id)) },
+  });
+  console.log(ingredientsList);
+  const result = await Recipe.create({
+    owner,
+    thumb: path,
+    ...recipe,
+    ingredients: ingredientsList,
+  });
   res.status(201).json(result);
 };
-// добавить рецепты пользователя
+
 module.exports = addOwnRecipes;
