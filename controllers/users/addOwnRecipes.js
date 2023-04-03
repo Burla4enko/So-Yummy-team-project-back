5d1288199647ca40d94d40cf59944355cc969b6a
Bordiuzhan <bordyujan@gmail.com>
2023-04-03 19:05:26 +0200

    refactoring

--- controllers/users/addOwnRecipes.js
+++ controllers/users/addOwnRecipes.js
@@ -6,14 +6,7 @@ const { ObjectId } = mongoose.Types;
 const addOwnRecipes = async (req, res) => {
   const { _id: owner } = req.user;
   const { path } = req.file;
-  const { ...recipe } = req.body;
-  const ingredients = [
-    '640c2dd963a319ea671e377f',
-    '640c2dd963a319ea671e36e4',
-    '640c2dd963a319ea671e377e',
-  ]; // потім деструктуризувати з req.body дивлячись які дані будуть приходити//
-
-  // додати перевірку на валідність Object Id
+  const { ingredients = [], ...recipe } = req.body;
 
   const ingredientsList = await Ingredient.find({
     _id: { $in: ingredients.map((id) => new ObjectId(id)) },
@@ -24,11 +17,13 @@ const addOwnRecipes = async (req, res) => {
     ...recipe,
     ingredients: ingredientsList,
   });
+
   const recipesList = await Recipe.find({ owner });
   if (recipesList.length === 10) {
-    res.json({ message: 'Yuhu, you already have 10 recipes' }); // При додаванні 10-го рецепту!!!!!
+    return res
+      .status(201)
+      .json({ result, message: 'Yuhu, you already have 10 recipes' }); // При додаванні 10-го рецепту!!!!!
   }
   res.status(201).json(result);
 };
-
 module.exports = addOwnRecipes;
