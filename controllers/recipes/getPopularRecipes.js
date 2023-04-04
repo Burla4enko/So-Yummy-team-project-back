e45701fa694e836be6694a4f8d5cac40f34a799e
Andrew Burlachenko <ap.burla4enko@gmail.com>
2023-04-05 01:26:25 +0300

    Update getPopularRecipes.js

--- controllers/recipes/getPopularRecipes.js
+++ controllers/recipes/getPopularRecipes.js
@@ -1,14 +1,11 @@
 const { Recipe } = require("../../models/recipe");
 
 const getPopularRecipes = async (req, res) => {
-    const result = await Recipe.find({});
-    const data = result.sort((a, b) => b.favorites.length - a.favorites.length).slice(0, 5)
-    res.json({
-        status: "success",
-        code: 200,
-        data: {
-            data,
-        }
-    })
+  const result = await Recipe.find({});
+  const data = result
+    .sort((a, b) => b.favorites.length - a.favorites.length)
+    .slice(0, 5);
+
+  res.json(data);
 };
 module.exports = getPopularRecipes;
