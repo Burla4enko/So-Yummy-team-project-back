aa3ec0b7a28bfe18c78a5638fef9b19e6f5f18a0
alexanderyar <103504393+alexanderyar@users.noreply.github.com>
2023-04-05 20:05:33 -0600

    delShoppingList now deleting an object from an array

--- controllers/users/delShoppingList.js
+++ controllers/users/delShoppingList.js
@@ -5,12 +5,19 @@ const delShoppingList = async (req, res) => {
   // getting shopping list item ID to delete from req.body
   const { id } = req.body;
   // deleting shopping list item drom the database by it's OWN id (not user id, not ingredient id)
-  const result = await ShoppingList.findByIdAndRemove(id);
+
+  // !!
+
+  // !! works !!!
+  const result = await ShoppingList.updateOne(
+    {},
+    { $pull: { list: { _id: id } } },
+    { new: true }
+  );
   if (!result) {
-    throw new NotFound(
-      "ShoppingList item has NOT found in the database, can't delete"
-    );
+    throw new NotFound("Not found, can't delete");
   }
+
   res.status(200).json({
     code: 200,
     message: "shopping list item has been deleted",
