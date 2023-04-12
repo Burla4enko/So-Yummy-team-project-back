946c84d8faa9477bc9bf251348ff8cf2689a908a
Andrew Burlachenko <ap.burla4enko@gmail.com>
2023-04-12 03:35:27 +0300

    Update addOwnRecipes.js

--- controllers/users/addOwnRecipes.js
+++ controllers/users/addOwnRecipes.js
@@ -1,10 +1,12 @@
-const { Recipe } = require('../../models/recipe');
-const mongoose = require('mongoose');
+const { Recipe } = require("../../models/recipe");
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Types;
 
 const addOwnRecipes = async (req, res) => {
   const { _id: owner } = req.user;
-  const { path } = req.file; /*Добавити заглушку не знаю де вона лежить*/
+  const {
+    path = "https://asset.cloudinary.com/dik8ehacz/04185f05f6e2f0607f21c67b9b1eab64",
+  } = req.file;
   const { ingredients = [], ...recipe } = req.body;
 
   const newIngredients = ingredients.map((ingredient) => ({
@@ -24,7 +26,7 @@ const addOwnRecipes = async (req, res) => {
   if (recipesList.length === 10) {
     return res
       .status(201)
-      .json({ result, message: 'Yuhu, you already have 10 recipes' }); // При додаванні 10-го рецепту!!!!!
+      .json({ result, message: "Yuhu, you already have 10 recipes" }); // При додаванні 10-го рецепту!!!!!
   }
   res.status(201).json(result);
 };
