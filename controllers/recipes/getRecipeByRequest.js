4ac7e8fd82067d86e0e005311b6d754b8ba1c93e
Daria-Zakharova <dariazakharova.smthelse@gmail.com>
2023-04-13 12:09:18 +0300

    Revert "add pagination"
    This reverts commit d593a53e4946ba6c732c8cb65e61e3a87217ef41.

--- controllers/recipes/getRecipeByRequest.js
+++ controllers/recipes/getRecipeByRequest.js
@@ -2,12 +2,8 @@ const { Recipe } = require("../../models/recipe");
 const { HttpError } = require("../../helpers");
 
 const getRecipeByRequest = async (req, res) => {
-  const { page = 1, limit = 6, query } = req.query;
-  const skip = (page - 1) * limit;
-  const searchRecipe = await Recipe.find({ $text: { $search: query } }, "", {
-    skip,
-    limit,
-  });
+  const { query } = req.query;
+  const searchRecipe = await Recipe.find({ $text: { $search: query } });
   if (searchRecipe.length === 0) {
     throw HttpError(404, "recipe not found");
   }
--- controllers/recipes/getRecipesByIngredients.js
+++ controllers/recipes/getRecipesByIngredients.js
@@ -3,8 +3,7 @@ const { Ingredient } = require("../../models/ingredient");
 const { HttpError } = require("../../helpers");
 
 const getRecipesByIngredients = async (req, res) => {
-  const { page = 1, limit = 6, query } = req.query;
-  const skip = (page - 1) * limit;
+  const { query } = req.query;
   const result = await Ingredient.find({
     $text: { $search: query },
   });
@@ -14,11 +13,7 @@ const getRecipesByIngredients = async (req, res) => {
   // const ingredientId = result[0]._id;
   const ingredientsId = result.map((item) => item._id);
 
-  const value = await Recipe.find(
-    { "ingredients.id": { $in: ingredientsId } },
-    "",
-    { skip, limit }
-  );
+  const value = await Recipe.find({ "ingredients.id": { $in: ingredientsId } });
   if (value.length === 0) {
     throw HttpError(404, "recipe not found");
   }
